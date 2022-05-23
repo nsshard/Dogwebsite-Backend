74d31d35ec7e2dab06cf1041f48dc494ebd0de09
nsshard <therealdude12@gmail.com>
2022-05-23 17:39:13 +0800

    Added APIs for image uploading/getting individual/getting all
    Added API for image/file processing, also deleted useless testing stuff (including some frontend elements that shouldnt be here)

--- src/users/models/users.model.js
+++
@@ -1,9 +0,0 @@
-const userModel = mongoose.model('Users', userSchema);
-
-const userSchema = new Schema({
-   firstName: String,
-   lastName: String,
-   email: String,
-   password: String,
-   permissionLevel: Number
-});
