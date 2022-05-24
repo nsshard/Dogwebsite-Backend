5d9ea19532edbc29ab4ac7d03f5163662dab6adb
nsshard <therealdude12@gmail.com>
2022-05-25 02:04:46 +0800

    Comments API + Logout API + user info (WIP, will be likely scrapped) API
    Added Comments API and Logout API. User info API is added but it remains buggy as of now, will likely be scrapped or have its code repurposed for other functions.

--- src/backend/model/Comments.js
+++ src/backend/model/Comments.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
+const commentSchema = new Schema({
+    name: {
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true
-    },
-    staffcode: {
+    commentmsg: {
         type: String,
         required: true
     },
     
+    
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Comment', commentSchema);
