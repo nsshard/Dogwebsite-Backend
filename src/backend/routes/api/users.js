a0977b92c8102291f3b248fa3990042d8f2b5823
nsshard <therealdude12@gmail.com>
2022-05-27 18:02:25 +0800

    Auth JWT stuff/openapi 2.0 doc/JSDOC
    new

--- src/backend/routes/api/users.js
+++ src/backend/routes/api/users.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../../controllers/usersController');
+const jwt_decode = require('jwt-decode');
+/**
+ * VerifyJWT that prevents user manipulation
+ * 
+ */
+const verifyJWT = (req, res, next) => {
+    console.log(req.headers);
+    const token = req.headers["cookie"]
+console.log(token);
+    if (!token) {
+        res.sendStatus(404);
+    } else {
+        var decoded = jwt_decode(token);
+        console.log(decoded); {
+        if (decoded.isAdmin != true) { 
+            return res.sendStatus(409); 
+            } else {
+               
+                next();
+            }
+        }
+    }
+};
+
+/**
+ * Routing functions
+ * 
+ */
 
 router.route('/')
-    .get(usersController.getAllUsers)
-    .delete(usersController.deleteUser);
+    .get(verifyJWT,usersController.getAllUsers)
+    .delete(verifyJWT,usersController.deleteUser);
 
 router.route('/:id')
     .get(usersController.getUser);
